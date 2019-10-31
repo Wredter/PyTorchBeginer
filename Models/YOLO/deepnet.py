72a82fff0df80d81c61f35748ddee0d9deeb2249
Wiktor <wiktor.muraszko@wp.pl>
2019-10-31 14:04:32 +0100

    Forrward pass for all layers exept YOLO layer

--- Models/YOLO/deepnet.py
+++ Models/YOLO/deepnet.py
@@ -14,4 +14,34 @@ class YOLODeepNet(nn.Module):
         self.net_info, self.module_list = create_modules(self.blocks)
 
     def forward(self, x, CUDA):
+        modules = self.blocks[1:]
+        print(self.blocks[1:])
+        outputs = {}
+        for i, module in enumerate(modules):
+            module_type = (module["type"])
+            if module_type == "convolutional" or module_type == "upsample":
+                #To pytorch umie samemu
+                x = self.module_list[i](x)
+            elif module_type == "route":
+                #Tego nie umie
+                layers = module["layers"]
+                layers = [int(a) for a in layers]
+
+                if (layers[0]) > 0:
+                    layers[0] = layers[0] - i
+
+                if len(layers) == 1:
+                    x = outputs[i + (layers[0])]
+                else:
+                    if(layers[1]>0):
+                        layers[1] = layers[1] - i
+                    map1 = outputs[i + layers[0]]
+                    map2 = outputs[i + layers[1]]
+
+                    x = torch.cat((map1, map2), 1)
+            elif module_type == "shortcut":
+                from_ = int(module["from"])
+                x = outputs[i-1] + outputs[i+from_]
+            #TODO YOLO layer,
+
         return
--- Test.py
+++ Test.py
@@ -1,12 +1,11 @@
 import torch
 from Models.YOLO.config.parser import parse_cfg, create_modules
+from Models.YOLO.deepnet import *
 import os
 
 x = os.getcwd()
 x += "\\Models\\YOLO\\config\\yolov3.cfg"
 print(x)
-
-y = parse_cfg(x)
-print(create_modules(y))
-
+YOLO = YOLODeepNet(x)
+#YOLO.forward(0, True)
 
